23380253fc06a30426f72303b1f0b756de14a5c2
MendesTK <murilo.begnini@univille.br>
2017-04-07 14:44:08 -0300

    alterações dia 07/04

--- JavaScript/teste/src/jogoMAT/testes.java
+++ JavaScript/teste/src/jogoMAT/testes.java
@@ -1,11 +1,23 @@
 package jogoMAT;
 
-public class testes {
-	
-	public static void main(String Args[]){
-		int random1;
-		random1 = (int) Math.floor(Math.random() * 10); 
-		System.out.println(random1);
-	}
+import java.util.Scanner;
 
+
+public class testes {
+
+	public static void main(String args[]){
+		int opcao = 0;
+		Scanner leitor = new Scanner(System.in);
+		do{
+			System.out.println("|================|");
+			System.out.println("|     OPCOES     |");
+			System.out.println("|================|");
+			System.out.println("| 1 - Repetir    |");
+			System.out.println("| 2 - Sair       |");
+			System.out.println("|================|");
+			opcao = leitor.nextInt();
+		}while(opcao != 2);
+		System.out.println("Fim do programa.");
+	}
+	
 }
