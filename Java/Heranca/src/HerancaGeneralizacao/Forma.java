7e999a24ea892c835ad3f5b7c34138ff53e4f4fd
MendesTK <murilo.begnini@univille.br>
2017-08-17 21:48:11 -0300

    .

--- Java/Heranca/src/HerancaGeneralizacao/Forma.java
+++ Java/Heranca/src/HerancaGeneralizacao/Forma.java
@@ -2,23 +2,8 @@ package HerancaGeneralizacao;
 
 public class Forma {
 	
-	protected String id;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
 	public double calcularVolume(){
 		return 0;
-	}
-	
-	@Override
-	public String toString(){
-		return id;
 	}
 
 }
