c89df5d8a14e218a2edc96432838e0fbfc9ad5cc
MendesTK <murilo.begnini@univille.br>
2017-09-22 09:48:29 -0300

    .

---
+++ Java/teste/src/Rieper/CriarArray.java
@@ -0,0 +1,13 @@
+package Rieper;
+
+import java.util.ArrayList;
+
+public class CriarArray {
+
+	public static void main(String[] args) {
+		String Pergunta1 = "Poupança, Consórcio";
+		String[] palavra = Pergunta1.split(",");
+		System.out.println(palavra[1]);
+	}
+
+}
