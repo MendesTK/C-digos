30cabdebc446d717043e0ab014d6e023fd987f57
MendesTK <murilo.begnini@univille.br>
2017-11-21 13:54:20 -0200

    .

--- Java/McDonalds/src/com/mcdonalds/modelo/Produtos.java
+++ Java/McDonalds/src/com/mcdonalds/modelo/Produtos.java
@@ -2,17 +2,17 @@ package com.mcdonalds.modelo;
 
 public class Produtos {
 
-	private int id_Produto;
+	private Integer id_Produto;
 	private String nome_Produto;
-	private float valor_Unit;
-	private float desconto;
+	private Float valor_Unit;
+	private Float desconto;
 	private String combo;
 	
 	public Produtos(){
 		
 	}
 	
-	public Produtos(int id_Produto, String nome_Produto, float valor_Unit, float desconto, String combo){
+	public Produtos(Integer id_Produto, String nome_Produto, Float valor_Unit, Float desconto, String combo){
 		this.id_Produto = id_Produto;
 		this.nome_Produto = nome_Produto;
 		this.valor_Unit = valor_Unit;
@@ -20,22 +20,22 @@ public class Produtos {
 		this.combo = combo;
 		
 	}
-	public Produtos(String nome_Produto, float valor_Unit, float desconto, String combo){
+	public Produtos(String nome_Produto, Float valor_Unit, Float desconto, String combo){
 		this.nome_Produto = nome_Produto;
 		this.valor_Unit = valor_Unit;
 		this.desconto = desconto;
 		this.combo = combo;
 		
 	}
-	public Produtos(int id_Produto){
+	public Produtos(Integer id_Produto){
 		this.id_Produto = id_Produto;
 	}
 	
 	
-	public int getId_Produto() {
+	public Integer getId_Produto() {
 		return this.id_Produto;
 	}
-	public void setId_Produto(int id_Produto) {
+	public void setId_Produto(Integer id_Produto) {
 		this.id_Produto = id_Produto;
 	}
 	public String getNome_Produto() {
@@ -44,16 +44,16 @@ public class Produtos {
 	public void setNome_Produto(String nome_Produto) {
 		this.nome_Produto = nome_Produto;
 	}
-	public float getValor_Unit() {
+	public Float getValor_Unit() {
 		return this.valor_Unit;
 	}
-	public void setValor_Unit(float valor_Unit) {
+	public void setValor_Unit(Float valor_Unit) {
 		this.valor_Unit = valor_Unit;
 	}
-	public float getDesconto() {
+	public Float getDesconto() {
 		return this.desconto;
 	}
-	public void setDesconto(float desconto) {
+	public void setDesconto(Float desconto) {
 		this.desconto = desconto;
 	}
 	public String getCombo() {
