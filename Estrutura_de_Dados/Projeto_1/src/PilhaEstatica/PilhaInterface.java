45eaa6c51730f69b806e70464ff39cf7779ae5c7
MURILO HENRIQUE DA LUZ BEGNINI <murilo.begnini@univille.br>
2018-04-20 20:03:05 -0300

    .

---
+++ Estrutura_de_Dados/Projeto_1/src/PilhaEstatica/PilhaInterface.java
@@ -0,0 +1,10 @@
+package PilhaEstatica;
+
+public interface PilhaInterface {
+	public void push(Object o) throws Exception;
+	public Object pop() throws Exception;
+
+	public boolean isEmpty();
+	public boolean isFull();
+	public void clear();
+}
