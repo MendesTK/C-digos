0e0d355edd89ac31705ec6229746be0293c4ea54
1111007520 <1111007520@LAB-B121-13.univille.br>
2018-08-03 21:45:45 -0300

    .

---
+++ Estrutura de Dados/Projeto_1/src/ComplexidadeDeAlgoritmo/main.java
@@ -0,0 +1,12 @@
+package ComplexidadeDeAlgoritmo;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		int v[] = new int[n];
+
+	}
+
+}
---
+++ Estrutura de Dados/Projeto_1/src/ComplexidadeDeAlgoritmo/minmax.java
@@ -0,0 +1,17 @@
+package ComplexidadeDeAlgoritmo;
+
+public class minmax {
+
+	public int vetorCrescente(int n){
+		int v[] = new int[n];
+		for(int i=0; i < n; i++){
+			v[i] = n;
+			n++;
+		}
+		
+		
+		return v;
+		
+	}
+	
+}
