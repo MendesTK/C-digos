9266b81e7ebbb797e0859bc51cbd1a7fd59dd43e
1111007520 <1111007520@LAB-B121-13.univille.br>
2018-08-10 22:17:46 -0300

    Ordenador de vetores finalizado.

---
+++ Estrutura de Dados/Projeto_1/src/ComplexidadeDeAlgoritmo/OrdenadorDeVetores.java
@@ -0,0 +1,124 @@
+package ComplexidadeDeAlgoritmo;
+
+public class OrdenadorDeVetores {
+
+	public OrdenadorDeVetores() {
+
+	}
+
+	public int[] gerarCrescente(int n) {
+		int v[] = new int[n];
+		int aux = 1;
+		for (int i = 0; i < n; i++) {
+			v[i] = aux;
+			aux += 1;
+			//System.out.println(v[i]);
+		}
+		return v;
+	}
+
+	public int[] gerarDecrescente(int n) {
+		int v[] = new int[n];
+		int aux = n;
+		for (int i = 0; i < n; i++) {
+			v[i] = aux;
+			aux -= 1;
+			//System.out.println(v[i]);
+		}
+		return v;
+	}
+
+	public int[] gerarAleatorio(int n) {
+		int v[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			int random = (int) Math.floor(Math.random() * n + 1);
+			v[i] = random;
+			//System.out.println(v[i]);
+		}
+		return v;
+
+	}
+
+	public int minmax1(int v[]) {
+		int i;
+		int cont1 = 0;
+		int min = v[0];
+		int max = v[0];
+		for (i = 1; i < v.length; i++) {
+			cont1 += 1;
+			if (v[i] > max) {
+				max = v[i];
+			}
+			cont1 += 1;
+			if (v[i] < min) {
+				min = v[i];
+			}
+
+		}
+		//System.out.println("1: Min: " + min + " | Max: " + max);
+		// return new int[] { min, max };
+		return cont1;
+	}
+
+	public int minmax2(int[] v) {
+		int i;
+		int cont2 = 0;
+		int min = v[0];
+		int max = v[0];
+		for (i = 1; i < v.length; i++) {
+			cont2 += 1;
+			if (v[i] > max) {
+				max = v[i];
+			} else {
+				cont2 += 1;
+				if (v[i] < min) {
+					min = v[i];
+				}
+			}
+		}
+		//System.out.println("2: Min: " + min + " | Max: " + max);
+		// return new int[] { min, max };
+		return cont2;
+	}
+
+	public int minmax3(int v[]) {
+		int i, a, b;
+		int cont3 = 0;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int dif = v.length % 2 == 0 ? 0 : 1;
+		for (i = 0; i < v.length - dif; i += 2) {
+			cont3 += 1;
+			if (v[i] < v[i + 1]) {
+				a = i;
+				b = i + 1;
+			} else {
+				a = i + 1;
+				b = i;
+			}
+			cont3 += 1;
+			if (v[a] < min) {
+				min = v[a];
+			}
+			cont3 += 1;
+			if (v[b] > max) {
+				max = v[b];
+			}
+		}
+		cont3 += 1;
+		if (dif == 1) {
+			cont3 += 1;
+			if (v[v.length - 1] > max) {
+				max = v[v.length - 1];
+			}
+			cont3 += 1;
+			if (v[v.length - 1] < min) {
+				min = v[v.length - 1];
+			}
+		}
+		//System.out.println("3: Min: " + min + " | Max: " + max);
+		// return new int[] { min, max };
+		return cont3;
+	}
+
+}
--- Estrutura de Dados/Projeto_1/src/ComplexidadeDeAlgoritmo/main.java
+++ Estrutura de Dados/Projeto_1/src/ComplexidadeDeAlgoritmo/main.java
@@ -3,10 +3,24 @@ package ComplexidadeDeAlgoritmo;
 public class main {
 
 	public static void main(String[] args) {
-		Complexidade vetor = new Complexidade();
-		vetor.gerarAleatorio(10);
-		
-		
+		OrdenadorDeVetores vetor = new OrdenadorDeVetores();
+		int n = 1000;
+
+		for (int i = 1; i <= n; i++) {
+			int a[] = vetor.gerarAleatorio(i);
+			int c[] = vetor.gerarCrescente(i);
+			int d[] = vetor.gerarDecrescente(i);
+
+			int n1 = vetor.minmax1(c);
+			int n2 = vetor.minmax2(c);
+			int n3 = vetor.minmax2(d);
+			int n4 = vetor.minmax2(a);
+			int n5 = vetor.minmax3(c);
+
+			System.out.println(i + ";" + n1 + ";" + n2 + ";" + n3 + ";" + n4 + ";" + n5);
+		}
+
+		// System.out.println(": " + vetorGerado[0]);
 
 	}
 
